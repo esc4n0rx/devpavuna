1c92a327fa5df99681f04ff5cd876d3e89a48781
esc4n0rx <paulo.cunha@hortifruti.com.br>
2024-03-22 18:03:56 -0300

    Add app.json for Heroku CI

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+from flask_testing import TestCase
+from app import app, db 
+
+class MyTest(TestCase):
+
+    def create_app(self):
+        
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  
+        return app
+
+    def setUp(self):
+      
+        db.create_all()
+
+    def tearDown(self):
+        
+        db.session.remove()
+        db.drop_all()
+
+
+def test_example():
+    assert True 
