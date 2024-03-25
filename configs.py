55b49d8ea7a37093165b7b4c262749608f9f35b1
esc4n0rx <paulo.cunha@hortifruti.com.br>
2024-03-25 14:32:28 -0300

    remoção de senhas diretas do codigo

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 # Arquivo configs.py
 EMAIL_SETTINGS = {
-    'EMAIL_USER': 'contato.paulooliver9@outlook.com',
-    'EMAIL_PASSWORD': 'Thivi8090p',
+    'EMAIL_USER': '',
+    'EMAIL_PASSWORD': '',
     'SMTP_SERVER': 'smtp.office365.com',
     'SMTP_PORT': 587
 }
